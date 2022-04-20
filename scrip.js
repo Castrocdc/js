65af2453d95c7a2654bf49d875f548a275e9d602
castrocdc <livcdc@gmail.com>
2022-04-20 00:05:03 -0300

    menu interactivo de guarderia

---
+++ scrip.js
@@ -0,0 +1,7 @@
+const laHora = 300
+
+
+let guarderia =prompt ("ingrese horas de cuidado por dia")
+    guarderia = guarderia * laHora * 20
+
+    alert ("su cuota es de " + guarderia + " pesos al mes")
